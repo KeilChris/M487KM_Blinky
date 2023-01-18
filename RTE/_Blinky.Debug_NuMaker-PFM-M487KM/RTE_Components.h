069afb5246d9239f4a873eeaed88a1bbd7d0e8ab
Christopher Seidl <KeilChris@users.noreply.github.com>
2023-01-18 16:15:57 +0100

    Converted to csolution

---
+++ RTE/_Blinky.Debug_NuMaker-PFM-M487KM/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky.Debug+NuMaker-PFM-M487KM' 
+ * Target:  'Blinky.Debug+NuMaker-PFM-M487KM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "M480.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
